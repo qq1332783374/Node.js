1181c7b5069977bbb9c8ffe98632ed594511888f
qq1332783374 <1332783374@qq.com>
2020-12-08 11:54:53 +0800

    后端：添加健康检查 controller

--- node-web/src/controllers/index.js
+++ node-web/src/controllers/index.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const shopControllers = require('./shop');
-const chaosController = require('./chaos')
+const chaosController = require('./chaos');
+const healthController = require('./health');
 
 module.exports = async function initControllers () {
     const router = Router();
@@ -9,6 +10,8 @@ module.exports = async function initControllers () {
     router.use('/api/shop', await shopControllers());
     // 错误异常处理路由
     router.use('/api/chaos', await chaosController());
+    // 健康检查
+    router.use('/api/health', await healthController());
 
     return router;
 }
