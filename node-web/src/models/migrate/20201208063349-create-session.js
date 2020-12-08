15e9efa1758c87c2cd89cc76d8c27d210a4e13cc
qq1332783374 <1332783374@qq.com>
2020-12-08 14:41:02 +0800

    后端：session 表结构初始化

---
+++ node-web/src/models/migrate/20201208063349-create-session.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('session', {
+      sid: {
+        type: Sequelize.STRING(36),
+      },
+      expires: {
+        type: Sequelize.DATE,
+      },
+      data: {
+        type: Sequelize.TEXT,
+      },
+
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('session');
+  }
+};
