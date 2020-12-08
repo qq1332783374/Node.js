a6a15cc4174b7c0d3599d87e170646f575f52313
qq1332783374 <1332783374@qq.com>
2020-12-08 11:58:13 +0800

    添加异常错误页面

--- node-web/src/server.js
+++ node-web/src/server.js
@@ -14,8 +14,28 @@ async function bootstrap () {
     server.use('/moulds', express.static(mouldsDir));
     server.use(await initMiddlewares());
     server.use(await initControllers());
+    server.use(errorHandler);
     await promisify(server.listen.bind(server, port))();
     console.log(`> Started on port ${port}`);
+}
+
+/**
+ * 错误异常处理
+ * @param req
+ * @param res
+ * @param next
+ */
+function errorHandler (err, req, res, next) {
+    if (res.headerExtensions) {
+        // 如果是在返回响应结果时发生了异常，
+        // 那么交给 express 内置的 finalhandler 关闭链接
+        return next(err)
+    }
+
+    // 打印异常
+    console.error(err);
+    // 重定向到错误页面
+    req.redirect('/500.html');
 }
 
 // 监听未捕获的 Promise 异常，直接退出进程
